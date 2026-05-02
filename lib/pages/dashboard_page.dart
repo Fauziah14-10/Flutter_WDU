3fcc1f0d6f7e7d5143204fb407f1a4c6a6e00005
scarecr0wz <fadel.yulis123@gmail.com>
2026-05-02 17:58:52 +0700

    fix: ios update on building

--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -124,8 +124,9 @@ class _DashboardViewState extends State<_DashboardView>
           ],
         ),
       ),
-    );
-  }
+    ),
+  );
+}
 
   SliverAppBar _buildAppBar() {
     return SliverAppBar(
