e46f35c8d79e6e68e0dba4bb96342d1bb0f6b52a
scarecr0wz <fadel.yulis123@gmail.com>
2026-05-02 18:27:48 +0700

    feat: inisialisasi hive dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 import 'package:provider/provider.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'core/theme/app_theme.dart';
 import 'pages/login_page.dart';
 import 'pages/dashboard_page.dart';
@@ -25,6 +26,7 @@ import 'core/utils/logger.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
 
   if (kDebugMode) {
     FlutterError.onError = (details) {
